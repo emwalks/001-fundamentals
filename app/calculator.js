604fc12c8070a4b2bbf643fc205ee96d4a8aef68
emwalks <walkere@tcd.ie>
2019-03-17 17:52:01 +0000

    revision

--- app/calculator.js
+++ app/calculator.js
@@ -2,6 +2,15 @@ function add(a, b) {
     return a + b;
 }
 
+function subtract(a, b) {
+    return a - b;
+}
+
+multiply = (a, b) => a * b
+//this is Es6 sythax
+
 module.exports = {
-    add
+    add,
+    subtract,
+    multiply
 };
